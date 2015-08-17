2ba6281e43f2bffaa6bb5567fb33f71cfa9d1fab
lenovo <1501814877@qq.com>
2015-08-17 22:52:04 +0800

    增加网络请求

--- src/main/java/com/sjkj/myapplication/http/parser/BackResult.java
+++ src/main/java/com/sjkj/myapplication/http/parser/BackResult.java
@@ -3,7 +3,27 @@ package com.sjkj.myapplication.http.parser;
 /**
  * 用于定义返回结果集中共有的元素，比如result，code....之类
  */
-public class BackResult {
+public class BackResult<E> {
 
+    private String success;
+    private Error error;
+
+
+    /* (non-Javadoc)
+     * @see com.alipear.serverrequest.item.BaseResult#isSuccess()
+     */
+    public boolean isSuccess() {
+        return success!= null && success.compareTo("true") ==0;
+    }
+
+    public String getError() {
+        return error.getMessage();
+    }
+
+
+    public String getResponse() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
